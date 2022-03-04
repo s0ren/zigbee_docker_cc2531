6bcc93a9cde4473708a08acc3038c8221eee2156
Søren Magnusson <smag@tec.dk>
2022-03-04 08:58:52 +0100

    docker-compose ps

--- restart.sh
+++ restart.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 start=`date +%s`
 echo "Running restart..."
+
 docker-compose --compatibility down && docker-compose --compatibility up -d
+docker-compose ps
+
 end=`date +%s`
 echo -e "\nScript has reached its end after: " $((end-start)) " seconds"
 
